4bc769702e47d67dd05a27c069b9c1a334872e07
aatifahmad123 <b22ai002@iitj.ac.in>
2025-05-22 14:04:13 +0530

    call 28

--- Calls_Google_Cloud_TTS/audio_merge.py
+++ Calls_Google_Cloud_TTS/audio_merge.py
@@ -1,19 +1,19 @@
 from pydub import AudioSegment
 
-audio_files_folder = "call27/audios"
+audio_files_folder = "call28/audios"
 files = [audio_files_folder + "/synthesis(0).wav", audio_files_folder + "/synthesis(1).wav", audio_files_folder + "/synthesis(2).wav", audio_files_folder + "/synthesis(3).wav", audio_files_folder + "/synthesis(4).wav", audio_files_folder + "/synthesis(5).wav"]
-combined_audio_file = "call27/call27.wav"
+combined_audio_file = "call28/call28.wav"
 
 def merge_audio(files):
-    # Initialize an empty AudioSegment
+    # Initialize an  AudioSegment
     combined = AudioSegment.empty()
 
-    # Loop through the list of files and append each one to the combined AudioSegment
+    # append each file to the combined AudioSegment
     for file in files:
         audio = AudioSegment.from_wav(file)
         combined += audio
 
-    # Export the combined audio to a new file
+    # Export the combined audio
     combined.export(combined_audio_file, format="wav")
 
 merge_audio(files)
