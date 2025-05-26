a65a7bf2db3959db90d60e8b707c8cacb7a66629
aatifahmad123 <b22ai002@iitj.ac.in>
2025-05-26 16:19:18 +0530

    deepseek-r1 on ollama

---
+++ Call_Summarization_and_Emotional_Analysis/main.py
@@ -0,0 +1,56 @@
+from dotenv import load_dotenv
+import os
+from google import genai
+from google.genai import types
+import json
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+if not GEMINI_API_KEY:
+    raise ValueError("GEMINI_API_KEY not found")
+
+call_id = int(input("Enter the call ID: "))
+
+if call_id < 10:
+    call_id = f"0{call_id}"
+
+call_file = f"../Calls_Google_Cloud_TTS/call{call_id}/call{call_id}.json"
+
+transcript = ""
+with open(call_file, "r") as f:
+    transcript = json.load(f)
+
+print(f"Transcript: {transcript}")
+
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+prompt = (
+    "Analyze the following call transcript and respond in the following JSON format: "
+    "{ "
+    '"summary": "<One professional sentence, no more than 10 words>", '
+    '"key_topics": { '
+    '"products": [<list of products mentioned>], '
+    '"issues": [<list of issues discussed>], '
+    '"actions": [<list of actions taken or promised>] '
+    '}, '
+    '"query_type": "<Type of customer query>", '
+    '"resolution_status": "<Resolved/Unresolved/Partially resolved>", '
+    '"callback_promise": "<Yes/No/Not mentioned>", '
+    '"dominant_emotions (customer)": [<list main emotions expressed by the customer>], '
+    '"conversation sentiment": "<Overall sentiment of the conversation: Positive/Negative/Neutral>", '
+    '"agent_professionalism": "<Brief comment>" '
+    "} "
+    f"Transcript: {transcript}"
+)
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents=prompt,
+    config=types.GenerateContentConfig(
+        max_output_tokens=250,
+        temperature=0.2
+    )
+)
+
+print("Call summary:", response.text.strip())
