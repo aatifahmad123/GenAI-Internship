937f7db83d8bd891138c65f8888a1a46d4fa0f6f
aatifahmad123 <b22ai002@iitj.ac.in>
2025-06-18 14:44:36 +0530

    added all hinglish data

--- Synthetic Data Generation/dataFeeder.py
+++ Synthetic Data Generation/dataFeeder.py
@@ -1,7 +1,7 @@
 import json
 
 json_file_path = "dataHindi.json"
-working_dir = f'../Google Cloud Hindi'
+working_dir = f'../Google Cloud Hinglish'
 
 with open(json_file_path, "r", encoding="utf-8") as jsonf:
     data = json.load(jsonf)
